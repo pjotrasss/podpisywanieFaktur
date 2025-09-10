a604aa5bd2fc82ca3766b67d1fa5fa5ad346b6d2
pjotrasss <pjotrasss@WALUTY>
2025-09-10 16:08:16 +0200

    rebuilt methods, made GetPathFromUser more universal

--- podpisywanie faktur/FileHelper.cs
+++ podpisywanie faktur/FileHelper.cs
@@ -1,29 +1,71 @@
 ﻿using static InputValidationHelper;
+using System.ComponentModel;
 
 internal static class FileHelper
 {
-    internal static string GetPathFromUser(string message)
+    internal enum OutputPathMethod
+    {
+        [Description("Automatyczne utworzenie nazwy (dopisek _signed.xml)")] Auto = 1,
+        [Description("Ręczny wybór nazwy")] Manual = 2
+    }
+
+
+    internal static string GetPathFromUser(string message, bool mustExist)
     {
         while (true)
         {
+            //pobranie sciezki od usera
             Console.WriteLine(message);
             var userInput = Console.ReadLine();
 
+            //konwersja na sciezke absolutna
             var fullPath = ValidatePathInput(userInput);
             if (fullPath == null)
                 continue;
 
-            if (File.Exists(fullPath))
-                return fullPath;
+            //dla plikow wejsciowych - mustExist = true
+            if (mustExist)
+            {
+                if (File.Exists(fullPath))
+                    return fullPath;
 
-            Console.WriteLine($"Plik {fullPath} nie istnieje");
+                Console.WriteLine($"Plik {fullPath} nie istnieje");
+                continue;
+            }
+            //dla plikow wyjsciowych - mustExist = false
+            else
+            {
+                //walidacja rozszerzenia - dodaje jesli brak, podmienia na xml jesli bledne
+                var extension = Path.GetExtension(fullPath);
+                if (extension != ".xml")
+                {
+                    Console.WriteLine("Wykryto brak rozszerzenia pliku lub bledne rozszerzenie, zmiana na '.xml'");
+                    fullPath = Path.ChangeExtension(fullPath, ".xml");
+                }
+
+                if (!File.Exists(fullPath))
+                    return fullPath;
+
+                Console.WriteLine($"Plik {fullPath} już istnieje, czy chcesz go nadpisać? (t/n)");
+
+                if (!YesOrNo())
+                    continue;
+                return fullPath;
+            }
         }
     }
 
-
-
-    internal static string BuildOutputPath(string inputPath)
+    //dodac sprawdzenie czy plik istnieje
+    internal static string AutoOutputPath(string inputPath)
     {
-        return $"{Path.GetFileNameWithoutExtension(inputPath)}_signed.xml";
+        var fullPath = $"{Path.GetFileNameWithoutExtension(inputPath)}_signed.xml";
+        if (!File.Exists(fullPath))
+            return fullPath;
+
+        Console.WriteLine($"Plik {fullPath} już istnieje, czy chcesz go nadpisać? (t/n)");
+        
+        if (YesOrNo())
+            return fullPath;
+        return GetPathFromUser (inputPath, false);
     }
 }
