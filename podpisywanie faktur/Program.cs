ae2de0abaff92612f460f0ad3ac49270745684da
pjotrasss <pjotrasss@WALUTY>
2025-09-12 16:41:13 +0200

    redesing of app's logics

--- podpisywanie faktur/Program.cs
+++ podpisywanie faktur/Program.cs
@@ -1,24 +1,13 @@
-﻿using static CertificatesHelper;
-using static XmlSigner;
-using static FileHelper;
-using static ChoiceHelper;
+﻿using podpisywanie_faktur;
 using System.Security.Cryptography.X509Certificates;
+using static CertificatesHelper;
+using static ChoiceHelper;
+using static FileHelper;
+using static XmlSigner;
+using static podpisywanie_faktur.InvoiceFactory;
 
-//wybor faktury do podpisania
-var fullInputPath = GetPathFromUser("Podaj plik do podpisania i naciśnij Enter", true);
 
-//wybor rodzaju utworzenia nazwy podpisanej faktury
-OutputPathMethod outputMethod = ChoiceMenu<OutputPathMethod>("Wybierz, jak nazwać podpisaną fakturę i naciśnij Enter");
-
-//stworzenie nazwy podpisanej faktury
-var fullOutputPath = outputMethod switch
-{
-    OutputPathMethod.Auto => AutoOutputPath(fullInputPath),
-    OutputPathMethod.Manual => GetPathFromUser("Podaj nazwę lub pełną ścieżkę zapisu podpisanej faktury i naciśnij Enter", false),
-    _ => throw new InvalidOperationException($"Nieobsłużona wartość: {outputMethod}")
-};
-
-//wybor rodzaju wczytania certyfikatu
+//wybor sposobu wczytania certyfikatu
 CertLoadMethod loadMethod = ChoiceMenu<CertLoadMethod>("Wybierz sposób załadowania certyfikatu i naciśnij Enter");
 
 //wczytanie certyfikatu
@@ -29,13 +18,94 @@ X509Certificate2? certificate = loadMethod switch
     _ => throw new InvalidOperationException($"Nieobsłużona wartość: {loadMethod}")
 };
 
-if (certificate == null)
+if (!ValidateCertificate(certificate))
     return;
+
+
+
+//wybor formy podpisywania faktur
+InputMethod inputMethod = ChoiceMenu<InputMethod>("Wybierz sposób podpisania faktur i naciśnij Enter");
+
+//wybor faktury/folderu faktur do podpisania
+var fullInputPath = inputMethod switch
+{
+    InputMethod.SingleFile => GetPathFromUser("Podaj plik do podpisania i naciśnij Enter", true),
+    InputMethod.FullDir => GetDirFromUser("Podaj folder zawierający faktury do podpisania i naciśnij Enter"),
+    _ => throw new InvalidOperationException($"Nieobsłużona wartość: {inputMethod}")
+};
+
+
+if(Directory.Exists(fullInputPath))
+{
+    //lista nazw faktur w folderze wejsciowym
+    IEnumerable<Invoice> invoices = FromDirectory(fullInputPath);
+    //liczniki udanych i nieudanych operacji
+    int signed = 0;
+    int failed = 0;
+
+    foreach (var invoice in invoices)
+    {
+        try
+        {
+            //podpisanie pliku
+            await SignXmlAsync(invoice.inputPath, invoice.outputPath, certificate);
+
+            //nieudana weryfikacja podpisu
+            if (!VerifySignedXml(invoice.outputPath))
+            {
+                Console.WriteLine($"Błąd weryfikacji podpisu: {invoice.outputPath}");
+                failed++;
+            }
+
+            //udana weryfikacja podpisu
+            Console.WriteLine($"OK {invoice.outputPath}");
+            signed++;
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine($"Błąd: {invoice.outputPath} -> {exception.Message}");
+            failed++;
+        }
+    }
+
+    //podsumowanie, zakonczenie dzialania aplikacji
+    Console.WriteLine($"Zakończono podpisywanie z {failed} błędami i {signed} udanymi operacjami");
+    return;
+}
+else
+{
+    //wybor rodzaju utworzenia nazwy podpisanej faktury
+    OutputPathMethod outputMethod = ChoiceMenu<OutputPathMethod>("Wybierz, jak nazwać podpisaną fakturę i naciśnij Enter");
+
+    //stworzenie sciezki podpisanej faktury
+    var fullOutputPath = outputMethod switch
+    {
+        OutputPathMethod.Auto => AutoOutputPath(fullInputPath, false),
+        OutputPathMethod.Manual => GetPathFromUser("Podaj nazwę lub pełną ścieżkę zapisu podpisanej faktury i naciśnij Enter", false),
+        OutputPathMethod.AutoToFolder => AutoOutputPath(fullInputPath, true),
+        _ => throw new InvalidOperationException($"Nieobsłużona wartość: {outputMethod}")
+    };
+
 
     //podpisanie Xmla
-await SignXmlAsync(fullInputPath, fullOutputPath, certificate);
-if (!VerifySignedXml(fullOutputPath))
-    return;
+    try
+    {
+        //podpisanie
+        await SignXmlAsync(fullInputPath, fullOutputPath, certificate);
 
-Console.WriteLine($"OK {fullOutputPath}");
-return;
+        //nieudana weryfikacja podpisu
+        if (!VerifySignedXml(fullOutputPath))
+        {
+            Console.WriteLine("Błąd weryfikacji podpisu");
+            return;
+        }
+
+        //udana weryfikacja podpisu
+        Console.WriteLine($"OK {fullOutputPath}");
+        return;
+    }
+    catch (Exception exception)
+    {
+        Console.WriteLine($"Błąd: {exception.Message}");
+    }
+}
