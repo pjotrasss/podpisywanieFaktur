65caeff5195bc4fca73bb3a45b923b9f57e2b309
pjotrasss <pjotrasss@WALUTY>
2025-09-12 16:40:54 +0200

    invoice object with factory

---
+++ podpisywanie faktur/Invoice.cs
@@ -0,0 +1,7 @@
+﻿namespace podpisywanie_faktur;
+
+internal class Invoice
+{
+    public required string inputPath { get; set; }
+    public required string outputPath { get; set; }
+}
---
+++ podpisywanie faktur/InvoiceFactory.cs
@@ -0,0 +1,23 @@
+﻿using static FileHelper;
+
+namespace podpisywanie_faktur;
+
+internal class InvoiceFactory
+{
+    internal static IEnumerable<Invoice> FromDirectory(string directory)
+    {
+        var invoicePaths = Directory.EnumerateFiles(directory, "*.xml", SearchOption.TopDirectoryOnly);
+
+        foreach(var path in invoicePaths)
+        {
+            if (Path.GetFileNameWithoutExtension(path).EndsWith("_signed"))
+                continue;
+
+            yield return new Invoice
+            {
+                inputPath = path,
+                outputPath = AutoOutputPath(path, false),
+            };
+        }
+    }
+}
